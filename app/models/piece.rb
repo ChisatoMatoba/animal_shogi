0fb8d89f32ff4e40c096ea1ad83a3c6fafdd078d
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-22 21:39:49 +0900

    駒のルール定義

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -26,19 +26,47 @@ class Piece
 
   private
 
-  def lion_moves
-    # ライオンの動きを定義
+  # ライオンの動き
+  def lion_moves(current_position)
+    row, col = current_position
+    moves = []
+    (-1..1).each do |row_offset|
+      (-1..1).each do |col_offset|
+        next if row_offset.zero? && col_offset.zero?
+
+        moves << [row + row_offset, col + col_offset]
+      end
+    end
+    moves.select { |r, c| valid_position?(r, c) }
   end
 
-  def elephant_moves
-    # ゾウの動きを定義
+  # ゾウの動きを定義
+  def elephant_moves(current_position)
+    row, col = current_position
+    moves = [[-1, -1], [-1, 1], [1, -1], [1, 1]].map do |row_offset, col_offset|
+      [row + row_offset, col + col_offset]
+    end
+    moves.select { |r, c| valid_position?(r, c) }
   end
 
-  def giraffe_moves
-    # キリンの動きを定義
+  # キリンの動きを定義
+  def giraffe_moves(current_position)
+    row, col = current_position
+    moves = [[-1, 0], [1, 0], [0, -1], [0, 1]].map do |row_offset, col_offset|
+      [row + row_offset, col + col_offset]
+    end
+    moves.select { |r, c| valid_position?(r, c) }
   end
 
-  def chick_moves
-    # ひよこの動きを定義（プロモーション含む）
+  # ひよこの動きを定義
+  def chick_moves(current_position, owner)
+    row, col = current_position
+    row_offset = owner == :sente ? -1 : 1
+    [[row + row_offset, col]].select { |r, c| valid_position?(r, c) }
+  end
+
+  # 移動先が枠内にあるか
+  def valid_position?(row, col)
+    row.between?(0, 3) && col.between?(0, 2)
   end
 end
